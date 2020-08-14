e4d3bba934ced108cb2656c14d98a5ad837ca7fb
Nimmy The Great <Nimrod@reshef.com>
2020-08-14 11:25:38 +0300

    import in app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Router, Route, Switch, } from 'react-router-dom'
 import { createBrowserHistory, } from 'history'
 import Login from './components/Login/Login'
+import Admin from './components/Admin/Admin'
+import Company from './components/Company/Company'
 
 
 function App() {
@@ -10,7 +12,7 @@ function App() {
 
     return (
        <Router history={history}>
-           
+
        </Router>
     );
 }
